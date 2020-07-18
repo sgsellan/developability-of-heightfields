8073a7ca9a7531b3eadd0f2c1b5c21eeb7c7fbeb
sgsellan <sgsellan@gmail.com>
2020-07-18 11:04:34 -0400

    commented

--- mex/setup_mex.m
+++ mex/setup_mex.m
@@ -1,2 +1,6 @@
+% Build mex functions required to run our ADMM optimization. This assumes you have
+% already built gptoolbox so that libigl has been cloned into gptoolbox/mex/external/
+
+
 mex('argmin_X_full_mex.cpp','-I../gptoolbox/mex/external/libigl/include/','-I../gptoolbox/mex/external/libigl/external/eigen/','-V');
 mex('argmin_X_full_L2_mex.cpp','-I../gptoolbox/mex/external/libigl/include/','-I../gptoolbox/mex/external/libigl/external/eigen/','-V');
